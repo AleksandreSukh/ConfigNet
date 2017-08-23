5ee6daa80edefbde1b9c4b43dc83dd7828fa6d07
aleksandresukhitashvili@gmail.com <aleksandresukhitashvili@gmail.com>
2017-08-23 15:12:19 +0400

    added Project and AppSettingsHelper class

---
+++ ConfigNet/ConfigNet/AppSettingsHelper.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConfigNet
+{
+    public class AppSettingsHelper
+    {
+        public static int ConfigParseInt(string configKey, NameValueCollection settingsSource)
+        {
+            var stringValue = settingsSource[configKey];
+            int configValue;
+            if (!int.TryParse(stringValue, out configValue))
+                throw new ConfigurationErrorsException($"Couldn't Parse int value of config key:{configKey}");
+            return configValue;
+        }
+        public static bool ConfigParseBool(string configKey, NameValueCollection settingsSource)
+        {
+            var stringValue = settingsSource[configKey];
+            bool configValue;
+
+            if (!bool.TryParse(stringValue, out configValue))
+                throw new ConfigurationErrorsException($"Couldn't Parse bool value of config key:{configKey}");
+            return configValue;
+        }
+
+        public static string ConfigParseString(string configKey, NameValueCollection settingsSource)
+        {
+            var stringValue = settingsSource[configKey];
+            if (string.IsNullOrEmpty(stringValue))
+                throw new ConfigurationErrorsException($"Couldn't Parse string value of config key:{configKey}");
+            return stringValue;
+        }
+    }
+
+}
