81ce87bc56659ebf48982801ba3e45c285866d38
aleksandresukhitashvili@gmail.com <aleksandresukhitashvili@gmail.com>
2017-08-23 15:16:58 +0400

    Fixed some reference errors

--- ConfigNet/ConfigNet/AppSettingsHelper.cs
+++ ConfigNet/ConfigNet/AppSettingsHelper.cs
@@ -1,5 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Configuration;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -34,5 +35,4 @@ namespace ConfigNet
             return stringValue;
         }
     }
-
 }
